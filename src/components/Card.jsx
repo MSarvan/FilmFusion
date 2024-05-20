0bacfee8abf67596260c9cbd98f1be0b251c2995
Saravanan M <sarvan.m20@gmail.com>
2024-05-20 21:20:53 +0530

    homepage completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import "../styles/card.scss";
 
-const Card = () => {
+const Card = ({ title, poster, year, index }) => {
   return (
-    <div>Moviecard</div>
-  )
-}
+    <div className="each-card" key={index}>
+      <div className="img-box">
+        <img src={poster} alt="" />
+      </div>
+      <div className="movie-title">{title}</div>
+      <div className="year">{year}</div>
+      <div className="info-div">
+        <div className="info-button">More Info..</div>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,74 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import '../styles/homepage.scss'
-import { FaHotjar } from "react-icons/fa"; // <FaHotjar /> trending icon
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import "../styles/homepage.scss";
+import { FaStar } from "react-icons/fa";
+import axios from "axios";
+import Card from "../components/Card";
 
 const Homepage = () => {
-  return (
-    <div className='homepage-container'>
-        <Navbar />
-    </div>
-  )
-}
+  const API_KEY = "693677a4";
 
-export default Homepage
+  const [movies, setMovies] = useState([]);
+  const [series, setSeries] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await axios.get(
+        `http://www.omdbapi.com/?s=movie&type=movie&apikey=${API_KEY}`
+      );
+      let result = response?.data?.Search;
+      // console.log(result, 'movies data');
+      setMovies(result);
+    };
+
+    const fetchSeries = async () => {
+      const response = await axios.get(
+        `http://www.omdbapi.com/?s=series&type=series&apikey=${API_KEY}`
+      );
+      let result = response?.data?.Search;
+      // console.log(result, 'series data');
+      setSeries(result);
+    };
+
+    fetchMovies();
+    fetchSeries();
+  }, []);
+
+  return (
+    <div className="homepage-container">
+      <Navbar />
+      <div className="content-area">
+        <div className="caption">
+          <div className="trend-icon">
+            <FaStar />
+          </div>
+          <h2>TRENDING TODAY</h2>
+        </div>
+        <div className="cards-data">
+          {movies?.map((e, i) => {
+            return (
+              <Card
+                title={e?.Title}
+                poster={e?.Poster}
+                year={e?.Year}
+                index={e?.imdbID}
+              />
+            );
+          })}
+          {series?.map((e, i) => {
+            return (
+              <Card
+                title={e?.Title}
+                poster={e?.Poster}
+                year={e?.Year}
+                index={e?.imdbID}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
