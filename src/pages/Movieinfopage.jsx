8bd4c8e949ba29985428ef56ce14a7d0c9fec6f2
Saravanan M <sarvan.m20@gmail.com>
2024-05-22 21:00:04 +0530

    movie info page UI updated

--- src/pages/Movieinfopage.jsx
+++ src/pages/Movieinfopage.jsx
@@ -1,12 +1,83 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import '../styles/movieinfopage.scss';
+import "../styles/movieinfopage.scss";
 
 const Movieinfopage = () => {
   return (
     <div className="movieinfopage-container">
       <Navbar />
-      <div>Movieinfopage</div>
+      <div className="movieinfo-content-area">
+        <div className="left-area">
+          <img
+            src="https://m.media-amazon.com/images/M/MV5BMTg4MDk1ODExN15BMl5BanBnXkFtZTgwNzIyNjg3MDE@._V1_SX300.jpg"
+            alt=""
+          />
+        </div>
+        <div className="right-area">
+          <h1>Title</h1>
+          <div className="seperator-div">
+            <div className="left-div">
+              <span>Released on:</span>
+              <span>Released</span>
+            </div>
+            <div className="left-div">
+              <span>Runtime:</span>
+              <span>Runtime</span>
+            </div>
+          </div>
+          <div className="seperator-div">
+            <div>
+              <span>Genre:</span>
+              <span>Genre</span>
+            </div>
+            <div>
+              <span>Director:</span>
+              <span>Director</span>
+            </div>
+          </div>
+          <div className="seperator-div">
+            <div>
+              <span>Writer:</span>
+              <span>Writer</span>
+            </div>
+            <div>
+              <span>Actors:</span>
+              <span>Actors</span>
+            </div>
+          </div>
+          <div>
+            <span>Plot:</span>
+            <span>Plot</span>
+          </div>
+          <div className="seperator-div">
+            <div>
+              <span>Language:</span>
+              <span>Language</span>
+            </div>
+            <div>
+              <span>Country:</span>
+              <span>Country</span>
+            </div>
+          </div>
+          <div>
+            <span>Awards:</span>
+            <span>Awards</span>
+          </div>
+          <div>Ratings:</div>
+          <div>
+            <span>Metascore:</span>
+            <span>Metascore</span>
+          </div>
+          <div>
+            <span>IMdB:</span>
+            <span>Metascore</span>
+          </div>
+          <div>
+            <span>BoxOffice collection:</span>
+            <span>BoxOffice</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
