6c18db0d90b4957c61b80b54610b0c23abb5a86a
Saravanan M <sarvan.m20@gmail.com>
2024-05-23 18:04:17 +0530

    movieinfo page updated

--- src/pages/Movieinfopage.jsx
+++ src/pages/Movieinfopage.jsx
@@ -1,80 +1,131 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import "../styles/movieinfopage.scss";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 const Movieinfopage = () => {
+  const API_KEY = "693677a4";
+  const { pathname } = useLocation();
+  const [movieInfo, setMovieInfo] = useState([]);
+  const [id, setId] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMovieDetails = async () => {
+      setIsLoading(true);
+      if (id) {
+        const response = await axios.get(
+          `http://www.omdbapi.com/?i=${id}&apikey=${API_KEY}`
+        );
+        setMovieInfo(response?.data);
+        setIsLoading(false);
+      }
+    };
+
+    fetchMovieDetails();
+  }, [id]);
+
+  const extractIdFromPathname = () => {
+    if (pathname) {
+      const temp = pathname.split("/").pop();
+      if (temp !== "") {
+        setId(temp);
+      }
+    }
+  };
+
+  useEffect(() => {
+    extractIdFromPathname();
+  }, [pathname]);
+
+  const handleClick = (title) => {
+    window.open(
+      `https://www.youtube.com/results?search_query=${title}+trailer`,
+      "_blank"
+    );
+  };
+
   return (
     <div className="movieinfopage-container">
       <Navbar />
       <div className="movieinfo-content-area">
         <div className="left-area">
-          <img
-            src="https://m.media-amazon.com/images/M/MV5BMTg4MDk1ODExN15BMl5BanBnXkFtZTgwNzIyNjg3MDE@._V1_SX300.jpg"
-            alt=""
-          />
+          <img src={movieInfo?.Poster} alt="Poster" />
         </div>
         <div className="right-area">
-          <h1>Title</h1>
+          <h1>{movieInfo?.Title}</h1>
           <div className="seperator-div">
             <div className="left-div">
-              <span>Released on:</span>
-              <span>Released</span>
+              <span>📅 Released on:</span>
+              <span>{movieInfo?.Released}</span>
             </div>
             <div className="left-div">
-              <span>Runtime:</span>
-              <span>Runtime</span>
+              <span>⏰ Runtime:</span>
+              <span>{movieInfo?.Runtime}</span>
             </div>
           </div>
           <div className="seperator-div">
             <div>
-              <span>Genre:</span>
-              <span>Genre</span>
+              <span>🎭 Genre:</span>
+              <span>{movieInfo?.Genre}</span>
             </div>
             <div>
-              <span>Director:</span>
-              <span>Director</span>
+              <span>🎬 Director:</span>
+              <span>{movieInfo?.Director}</span>
             </div>
           </div>
-          <div className="seperator-div">
-            <div>
-              <span>Writer:</span>
-              <span>Writer</span>
+          <div className="plot-div">
+            <span>✍ Writer:</span>
+            <span>{movieInfo?.Writer}</span>
+          </div>
+          <div className="plot-div">
+            <span>👨🏻‍🎤 Actors:</span>
+            <span>{movieInfo?.Actors}</span>
+          </div>
+          <div className="plot-div">
+            <span>
+              {" "}
+              <span>📝</span> Plot:
+            </span>
+            <span>{movieInfo?.Plot}</span>
+          </div>
+          <div className="plot-div">
+            <span>🔠 Language:</span>
+            <span>{movieInfo?.Language}</span>
+          </div>
+          <div className="plot-div">
+            <span>🌍 Country:</span>
+            <span>{movieInfo?.Country}</span>
+          </div>
+          <div className="plot-div">
+            <span>🥇 Awards:</span>
+            <span>{movieInfo?.Awards}</span>
+          </div>
+          <div className="ratings-div">
+            <div>🌟 Ratings:</div>
+            <div className="reviewer-div">
+              <div>
+                <span>Metascore:</span>
+                <span>{movieInfo?.Metascore}</span>
+              </div>
+              <div>
+                <span>IMdB:</span>
+                <span>{movieInfo?.imdbRating}</span>
+              </div>
             </div>
-            <div>
-              <span>Actors:</span>
-              <span>Actors</span>
-            </div>
           </div>
-          <div>
-            <span>Plot:</span>
-            <span>Plot</span>
+          <div className="plot-div">
+            <span>💰 BoxOffice collection:</span>
+            <span>{movieInfo?.BoxOffice}</span>
           </div>
-          <div className="seperator-div">
-            <div>
-              <span>Language:</span>
-              <span>Language</span>
-            </div>
-            <div>
-              <span>Country:</span>
-              <span>Country</span>
-            </div>
-          </div>
-          <div>
-            <span>Awards:</span>
-            <span>Awards</span>
-          </div>
-          <div>Ratings:</div>
-          <div>
-            <span>Metascore:</span>
-            <span>Metascore</span>
-          </div>
-          <div>
-            <span>IMdB:</span>
-            <span>Metascore</span>
-          </div>
-          <div>
-            <span>BoxOffice collection:</span>
-            <span>BoxOffice</span>
+          <div
+            className="trailer-button"
+            onClick={() => {
+              handleClick(movieInfo?.Title);
+            }}
+          >
+            Watch Trailer
           </div>
         </div>
       </div>
