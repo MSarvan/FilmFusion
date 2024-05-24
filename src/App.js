c36251fe136736c579b7209fd886336977a6cb87
Saravanan M <sarvan.m20@gmail.com>
2024-05-24 12:28:32 +0530

    theme added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Moviespage from "./pages/Moviespage";
@@ -6,15 +7,30 @@ import Movieinfopage from "./pages/Movieinfopage";
 import Seriesinfopage from "./pages/Seriesinfopage";
 
 function App() {
-  return <div className="App">
-    <Routes>
-      <Route path="/" element={<Homepage/>}/>
-      <Route path="/movies" element={<Moviespage />}/>
-      <Route path="/series" element={<Seriespage />}/>
-      <Route path="/movies/:id" element={<Movieinfopage />} />
-      <Route path="/series/:id" element={<Seriesinfopage />} />
-    </Routes>
-  </div>;
+  const [theme, setTheme] = useState("dark-mode");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) =>
+      prevTheme === "dark-mode" ? "light-mode" : "dark-mode"
+    );
+  };
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
+  return (
+    <div className={`App ${theme}`}>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/movies" element={<Moviespage />} />
+        <Route path="/series" element={<Seriespage />} />
+        <Route path="/movies/:id" element={<Movieinfopage />} />
+        <Route path="/series/:id" element={<Seriesinfopage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
