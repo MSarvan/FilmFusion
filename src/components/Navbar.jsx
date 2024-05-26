bb5a11e9e147de41e61b7ff66020bff63b13552a
Saravanan M <sarvan.m20@gmail.com>
2024-05-26 12:21:20 +0530

    cc

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "../styles/navbar.scss";
 import { FaVideo } from "react-icons/fa6";
 import { MdOndemandVideo } from "react-icons/md";
 import { SiSteelseries } from "react-icons/si";
 import { FaSearch } from "react-icons/fa";
 import { FaTimes } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { MdDarkMode } from "react-icons/md";
 import { MdLightMode } from "react-icons/md";
@@ -20,6 +21,8 @@ const Navbar = () => {
     isSearching,
     setIsSearching,
   } = useContext(MainContext);
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleSearch = () => {
     setIsSearching(!isSearching);
@@ -41,10 +44,34 @@ const Navbar = () => {
         </div>
         <h2>FILMFUSION</h2>
       </div>
+      <div className="nav-item nav-input">
+        <input
+          type="text"
+          placeholder="Search your favourites.."
+          onChange={(e) => {
+            setSearchParam(e.target.value);
+          }}
+          value={searchParam}
+        />
+        <div
+          className="icon-div"
+          onClick={handleSearch}
+          style={{ cursor: "pointer" }}
+        >
+          {isSearching ? <FaTimes /> : <FaSearch />}
+        </div>
+      </div>
+      <div
+        className="hamburger-menu"
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        {isMenuOpen ? <FaTimes /> : <FaBars />}
+      </div>
       <div
         className="nav-item"
         onClick={() => {
           navigate("/movies");
+          setIsMenuOpen(false);
         }}
       >
         <div className="icon-div">
@@ -56,6 +83,7 @@ const Navbar = () => {
         className="nav-item"
         onClick={() => {
           navigate("/series");
+          setIsMenuOpen(false);
         }}
       >
         <div className="icon-div">
@@ -63,23 +91,12 @@ const Navbar = () => {
         </div>
         <div>SERIES</div>
       </div>
-      <div className="nav-item nav-input">
-        <input
-          type="text"
-          placeholder="Search your favourites.."
-          onChange={(e) => {
-            setSearchParam(e.target.value);
-          }}
-          value={searchParam}
-        />
-        <div className="icon-div" onClick={handleSearch} style={{cursor: 'pointer'}}>
-          {isSearching ? <FaTimes /> : <FaSearch />}
-        </div>
-      </div>
+
       <div
         className="nav-item"
         onClick={() => {
           toggleTheme();
+          setIsMenuOpen(false);
         }}
       >
         <div className="icon-div">
